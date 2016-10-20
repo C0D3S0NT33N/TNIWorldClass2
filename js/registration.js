da28542d7ecdd2b3a2106e782f37cfb664aaec6f
Natthasak Vechprasit <C0D3S0NT33N@users.noreply.github.com>
2016-10-20 13:11:11 +0700

    Update registration.js

--- js/registration.js
+++ js/registration.js
@@ -1,3 +1,30 @@
+/************************************************************/
+/* TNI World Class #2
+/************************************************************/
+/* Organization: Thai-Nichi Institute of Technology
+/************************************************************/
+/* File: mainland.js
+/* Author/Developer: Natthasak Vechprasit
+/* Contributor:      Nat Phattano
+/* Design Engineer:  Natthasak Vechprasit
+/* Art Director: Yasumin Tamrareang
+/*               Thanawat Wanwarothorn
+/* Graphic Art:  Yasumin Tamrareang
+/*               Amita Chalearmsuk
+/************************************************************/
+/* Front-End Programmer: Natthasak Vechprasit
+/* Back-End Programmer:  Natthasak Vechprasit (System)
+/*                       Nat Phattano (Application)
+/* Co-Ordinator:         Kasidit Pathomkul
+/* Thank you to all testers who gave time to test the website
+/************************************************************/
+/* TangentRoute
+/************************************************************/
+/* For work with our team please contact
+/* E-Mail: ve.natthasak_st@tni.ac.th (Natthasak)
+/* Website: tangentroute.com
+/************************************************************/
+
 function readURL(a) {
     if (a.files && a.files[0]) {
         var b = new FileReader;
